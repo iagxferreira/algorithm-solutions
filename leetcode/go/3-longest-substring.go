1f9eeb34418d2045e7a55aae115c6d09c333b785
Iago Ferreira <iago-ferreira@outlook.com>
2025-07-11 13:49:40 -0300

    feat: added longest substring in go

---
+++ leetcode/go/3-longest-substring.go
@@ -0,0 +1,17 @@
+func lengthOfLongestSubstring(s string) int {
+	right := len(s)
+	max := 0
+	lastIndex := make([]int, 128)
+	start := 0
+	for left := 0; left < right; left++ {
+		current := s[left]
+		if lastIndex[current] > 0 {
+			start = lastIndex[current]
+		}
+		if left-start+1 > max {
+			max = left - start + 1
+		}
+		lastIndex[current] = left + 1
+	}
+	return max
+}
