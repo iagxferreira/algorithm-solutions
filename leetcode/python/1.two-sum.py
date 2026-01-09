1d5b52c4df7f9ad7bbd134726a34f28c0800c773
Iago Ferreira <iago-ferreira@outlook.com>
2026-01-09 18:49:39 -0300

    feat: adding two sum solved in python

---
+++ leetcode/python/1.two-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pairs = {}
+        for index, num in enumerate(nums):
+            if target - num in pairs:
+                return [index, pairs[target - num]]
+            pairs[num] = index
